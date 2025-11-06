a93040325d6271c1e27c3fc29e7c2402656e356c
Abhinav Singh <a1.abhinavsingh@gmail.com>
2025-11-06 17:06:07 +0100

    OpenTelemetry Integration in Backend

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,6 +4,11 @@ from .routes import auth_routes, claims_routes
 from .db import init_db
 from fastapi.middleware.cors import CORSMiddleware
 
+from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
+from opentelemetry.exporter.otlp.proto.grpc.exporter import OTLPSpanExporter
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+
 app = FastAPI(title="DICS Backend - Aurelia Labs", version="0.1.0")
 
 app.include_router(auth_routes.router)
@@ -24,3 +29,11 @@ async def startup_event():
 @app.get("/")
 def root():
     return {"message": "DICS Backend (Aurelia Labs)"}
+
+
+# Setup OpenTelemetry
+trace_provider = TracerProvider()
+otlp_exporter = OTLPSpanExporter(endpoint="http://otel-collector:4317", insecure=True)
+trace_provider.add_span_processor(BatchSpanProcessor(otlp_exporter))
+FastAPIInstrumentor.instrument_app(app, tracer_provider=trace_provider)
+
