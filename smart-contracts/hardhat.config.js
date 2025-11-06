31b5b5d47df2be21033a590b1bfa0d7de01371d7
Abhinav Singh <a1.abhinavsingh@gmail.com>
2025-11-06 10:42:17 +0100

    hardhat.config.js updated

--- smart-contracts/hardhat.config.js
+++ smart-contracts/hardhat.config.js
@@ -1,7 +1,13 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("solidity-coverage");
 
 module.exports = {
   solidity: "0.8.24",
+  networks: {
+    hardhat: {
+      chainId: 1337
+    }
+  },
   paths: {
     sources: "./contracts",
     tests: "./test",
